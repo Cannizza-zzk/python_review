58ef1457d938e40210fdf95df6fba07ff0ff0307
cannizza <cannizzacn@gmail.com>
2021-05-18 09:32:28 +0800

    medium

--- leetcode/954.py
+++ leetcode/954.py
@@ -13,4 +13,7 @@ class Solution:
         return True
 
 # python 中使用in查找某个元素，字典查找的速度>>列表查找的速度
+# list的存储形式是线性表，查找的效率其实是O(n)；dict的存储形式是哈希表，查找的效率是O(1)
+# 二者执行效率上相差了近10000倍
+# reference: https://blog.csdn.net/jmh1996/article/details/78481365
         
