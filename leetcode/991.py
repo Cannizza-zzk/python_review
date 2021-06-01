ec2e1c2ca1240c794e495ff94008f527e5ef356a
cannizza <cannizzacn@gmail.com>
2021-06-01 13:27:23 +0800

    medium

--- leetcode/991.py
+++ leetcode/991.py
@@ -1,7 +1,7 @@
 class Solution:
     def brokenCalc(self, x: int, y: int) -> int:
         if y <= x:
-            return y - x
+            return x - y
         else:
             if y % 2 == 0:
                 return 1 + self.brokenCalc(x,y//2)
