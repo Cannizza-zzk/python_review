3526145bb491cd76d447199414756648bccd436c
cannizza <cannizzacn@gmail.com>
2021-07-26 14:49:58 +0800

    delete

--- leetcode/week53d_1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countGoodSubstrings(self, s: str) -> int:
-        if len(s) < 3: return 0
-        ans  = 0
-        for i in range(len(s) - 2):
-            if len(collections.Counter(s[i:i+3])) == 3:
-                ans += 1
-        return ans
